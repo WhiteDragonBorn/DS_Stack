aeafd0fd01e245baf3e5611e95a8bf3f6a22de3b
SHIMANOEYE <shimanoee@yandex.ru>
2023-10-05 12:24:58 +0300

    update

--- DS_Stack.cpp
+++ DS_Stack.cpp
@@ -10,7 +10,43 @@
   Нельзя бегать по стеку.
 */
 
+/* Задачи на стеки:
+    1) Условие --- Закрытие скобок - есть (,),[,],{,}
+      Алгоритм:
+        Все символы не являющиеся скобками пропускаем.
+        Если встетилась открывающая скобка любого вида, то
+          помещаем её в стек.
+        Если встретилась закрывающая скобка, то
+          если стек на этот момент пуст, то return false;
+          если на вершине стека открывающая скобка другого вида, return false;
+          если на вершине стека парная открывающая скобка, то извлекаем ее из
+   стека; если строка закончилась, а стек не пуст, return false;
+
+   2) Выячисление значений постфиксных выражений
+
+   3) Построение постфиксной записи с помощью инфиксной
+    a - (b+c*d) / e
+    
+    ch | стек(от дна к вершине) | postfixexpr
+    a  |                        | a
+    -  | -                      | 
+    (  | -(                     |
+    b  | -(                     | ab
+    +  | -(+                    | ab
+    c  | -(+                    | abc
+    *  | -(+*                   | abc
+    d  | -(+*                   | abcd
+    )  | -(+                    | abcd*
+       | -(                     | abcd*+
+       | -                      | abcd*+
+    /  | -/                     | abcd*+
+    e  |                        | abcd*+/-
+
+*/
+
+#include <fstream>
 #include <iostream>
+#include <string>
 typedef int datatype;
 
 class TStack {
@@ -91,3 +127,28 @@ int main() {
   st.pop();
   std::cout << st.peek();
 }
+
+// Задача о скобках
+bool isCorrect(TStack st, std::string filename) {
+  bool result = true;
+  std::ifstream fin(filename);
+  char symbol;
+  while (fin >> symbol && result) {
+    if (symbol == '(' || symbol == '{' || symbol == '[')
+      st.push(symbol);
+    else {
+      if (symbol == ')' || symbol == '}' || symbol == ']') {
+        if (st.isEmpty() || ((symbol == ')') ^ (st.peek() == '(')) ||
+            ((symbol == '}') ^ (st.peek() == '{')) ||
+            ((symbol == ']') ^ (st.peek() == '['))) {
+          result = false;
+        } else {
+          st.pop();
+        }
+      }
+    }
+  }
+  return result && st.isEmpty();
+}
+
+// Задача о постфиксных выражениях
